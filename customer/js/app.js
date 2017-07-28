39288cf7e8c4c28a3e61d634f953790941ffa8b7
Foo Ru Jia <rujia.foo.2015@smu.edu.sg>
2017-07-28 11:03:34 +0800

    baseurl

--- customer/js/app.js
+++ customer/js/app.js
@@ -2,7 +2,7 @@
 var app = angular.module('crm', ['angular-storage']);
 
 //var baseurl = "http://localhost:7000/api/";
-var baseurl = "http://crm.fountaintechies.com:7000/api/";
+var baseurl = "http://crm.fountaintechies.com/api/";
 
 app.config(['storeProvider', function (storeProvider) {
   storeProvider.setStore('sessionStorage');
