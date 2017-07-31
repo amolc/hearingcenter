1bd1af8271bf3e6a2ad5f3e09f47b65b5fc59db8
Jhon Vryle Paulate <jhon2x015@gmail.com>
2017-07-31 13:09:59 +0800

    Admind: (Config) add config for localhost

--- admin/js/app.js
+++ admin/js/app.js
@@ -1,9 +1,27 @@
 'use strict';
 var app = angular.module('crm', ['angular-storage']);
 
-//var baseurl = "http://localhost:7000/api/";
-var baseurl = "http://crm.fountaintechies.com/api/";
 
-app.config(['storeProvider', function (storeProvider) {
-  storeProvider.setStore('sessionStorage');
-}]);
+if (document.location.hostname == "crm.fountaintechies.com") 
+{
+
+	var baseurl = "http://crm.fountaintechies.com/api/";
+
+	app.config(['storeProvider', function (storeProvider) {
+	  storeProvider.setStore('sessionStorage');
+	}]);
+
+}
+else
+{
+
+	var baseurl = "http://localhost:7000/api/";
+	//var baseurl = "http://crm.fountaintechies.com/api/";
+
+	app.config(['storeProvider', function (storeProvider) {
+	  storeProvider.setStore('sessionStorage');
+	}]);
+
+
+}
+
